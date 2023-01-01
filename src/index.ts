668f7afb64d0d32b49ec198a762a64a7e641dba5
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-12-31 18:02:57 -0800

    Added reset logic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 class Player {
     hand: Card[];
     total: number;
+    cardElements: HTMLElement[];
     constructor() {
         this.hand = [];
+        this.cardElements = [];
         this.total = 0;
     }
 }
 
 class Dealer {
-    hand: object[];
+    hand: Card[];
     total: number;
+    cardElements: HTMLElement[];
     constructor() {
         this.hand = [];
         this.total = 0;
+        this.cardElements = [];
     }
 }
 
@@ -38,6 +42,7 @@ class Deck {
         return new Card(suit, rank);
     }
     generateDeck() {
+        this.cards = [];
         for(const suit of this.suits) {
             for(const rank of this.ranks) {
                 this.cards.push(this.createCard(suit, rank))
@@ -45,7 +50,7 @@ class Deck {
         }
     }
     getCardIndex() {
-        const randomNumber: number = Math.floor(Math.random() * this.cards.length) + 1;
+        const randomNumber: number = Math.floor(Math.random() * this.cards.length);
         return randomNumber;
     }
 }
@@ -56,10 +61,13 @@ class Game {
     dealer: Dealer;
     playerScoreText: (HTMLParagraphElement|null);
     playerSection: (HTMLElement|null);
-    dealerScoreText: (HTMLElement|null);
+    dealerScoreText: (HTMLParagraphElement|null);
     dealerSection: (HTMLElement|null);
+    dealerFaceDownCard: (HTMLDivElement|null);
+    selectionsSection: (HTMLElement|null);
     hitButton: (HTMLButtonElement|null);
     stayButton: (HTMLButtonElement|null);
+    gameResultText: (HTMLParagraphElement|null);
     constructor() {
         this.deck = new Deck();
         this.player = new Player();
@@ -68,8 +76,11 @@ class Game {
         this.playerSection = document.querySelector('#player-section');
         this.dealerScoreText = document.querySelector('#dealer-score');
         this.dealerSection = document.querySelector('#dealer-section');
+        this.selectionsSection = document.querySelector('#selections-section');
         this.hitButton = document.querySelector('#hit-button');
         this.stayButton = document.querySelector('#stay-button');
+        this.gameResultText = document.querySelector('#game-result-text');
+        this.dealerFaceDownCard = document.querySelector('#dealer-face-down-card');
         if(this.hitButton !== null) {
             this.hitButton.addEventListener('click', () => {
                 this.drawPlayerCard();
@@ -77,19 +88,21 @@ class Game {
         }
         if(this.stayButton !== null) {
             this.stayButton.addEventListener('click', () => {
-                console.log('clicked stay');
+                this.initiateDealerTurn();
             })
         }
     }
     startNewGame() {
+        if(this.dealerFaceDownCard !== null) {
+            this.dealerFaceDownCard.style.display = 'block';
+        }
+        this.player.total = 0;
+        this.dealer.total = 0;
+        this.player.hand = [];
+        this.dealer.hand = [];
         this.deck.generateDeck();
         this.drawPlayerCard();
         this.drawPlayerCard();
-        const dealerFaceDownCard: HTMLDivElement = document.createElement('div');
-        dealerFaceDownCard.classList.add('card-container');
-        if(this.dealerSection !== null) {
-            this.dealerSection?.append(dealerFaceDownCard);
-        }
         this.drawDealerCard();
     }
     getRankValue(rank: (string|number), currentTurn: (Player|Dealer)) {
@@ -97,6 +110,7 @@ class Game {
         If the rank is 2-10, add the rank to the total. If the rank is Jack, Queen, or King add 10. If the rank is Ace and adding 11 would bust, then add 1, otherwise add 11
         */
         const numericValueRank: number = typeof rank === 'number' ? rank : 0;
+        console.log(rank, numericValueRank)
         switch (rank) {
             case 'A':
                 (currentTurn.total + 11) > 21 ?  currentTurn.total += 1 : currentTurn.total += 11;
@@ -117,20 +131,48 @@ class Game {
                 this.dealerScoreText.textContent = this.dealer.total.toString();
             }
         }
+        this.checkTotals(currentTurn);
+    }
+    initiateDealerTurn() {
+        if(this.dealerFaceDownCard !== null) {
+            this.dealerFaceDownCard.style.display = 'none';
+        }
+        if(this.stayButton !== null && this.hitButton !== null) {
+            this.hitButton.disabled = true;
+            this.stayButton.disabled = true;
+        }
+        if(this.dealer.total < 17) {
+            this.drawDealerCard();
+        }
+    }
+    resetBoard(startNewGameBtn: HTMLButtonElement) {
+        this.player.cardElements.forEach((card) => {
+            card.remove();
+        });
+        this.dealer.cardElements.forEach((card) => {
+            card.remove();
+        });
+        startNewGameBtn.remove();
+        if(this.hitButton && this.stayButton && this.gameResultText) {
+            this.hitButton.disabled = false;
+            this.stayButton.disabled = false;
+            this.gameResultText.textContent = '';
+        }
     }
     drawPlayerCard() {
-        const index: number = this.deck.getCardIndex();
-        const newCard: Card = this.deck.cards[index];
+        const index = this.deck.getCardIndex();
+        const newCard = this.deck.cards[index];
         this.player.hand.push(newCard);
-        this.getRankValue(newCard.rank, this.player)
-        const cardContainer: HTMLDivElement = document.createElement('div');
-        if(this.playerSection !== null) {
+        this.getRankValue(newCard.rank, this.player);
+        const cardContainer = document.createElement('div');
+        this.player.cardElements.push(cardContainer);
+        if (this.playerSection !== null) {
             this.playerSection.append(cardContainer);
             cardContainer.classList.add('card-container');
-            const cardSuit: HTMLParagraphElement = document.createElement('p');
+            const cardSuit = document.createElement('p');
             cardSuit.textContent = newCard.suit;
             cardSuit.classList.add('card-suit');
-            const cardRank: HTMLParagraphElement = document.createElement('p');
+            const cardRank = document.createElement('p');
             cardRank.textContent = newCard.rank.toString();
             cardRank.classList.add('card-rank');
             cardContainer.append(cardSuit);
@@ -139,24 +181,53 @@ class Game {
         this.deck.cards.splice(index, 1);
     }
     drawDealerCard() {
-        const index: number = this.deck.getCardIndex();
-        const newCard: Card = this.deck.cards[index];
+        const index = this.deck.getCardIndex();
+        const newCard = this.deck.cards[index];
         this.dealer.hand.push(newCard);
-        this.getRankValue(newCard.rank, this.dealer)
-        const cardContainer: HTMLDivElement = document.createElement('div');
-        if(this.dealerSection !== null) {
+        this.getRankValue(newCard.rank, this.dealer);
+        const cardContainer = document.createElement('div');
+        this.dealer.cardElements.push(cardContainer);
+        if (this.dealerSection !== null) {
             this.dealerSection.append(cardContainer);
             cardContainer.classList.add('card-container');
-            const cardSuit: HTMLParagraphElement = document.createElement('p');
+            const cardSuit = document.createElement('p');
             cardSuit.textContent = newCard.suit;
             cardSuit.classList.add('card-suit');
-            const cardRank: HTMLParagraphElement = document.createElement('p');
+            const cardRank = document.createElement('p');
             cardRank.textContent = newCard.rank.toString();
             cardRank.classList.add('card-rank');
             cardContainer.append(cardSuit);
             cardContainer.append(cardRank);
         }
         this.deck.cards.splice(index, 1);
+    }
+    checkTotals(currentTurn: (Player|Dealer)) {
+        if(currentTurn === this.player) {
+            if(this.player.total > 21) {
+                this.endGame('You lose, better luck next time!');
+            }
+        }
+        if(currentTurn === this.dealer) {
+            if(this.dealer.total > 21) {
+                this.endGame('You win, well done!');
+            }
+        }
+    }
+    endGame(resultText: string) {
+        if(this.hitButton !== null && this.stayButton !== null && this.gameResultText !== null) {
+            this.hitButton.disabled = true;
+            this.stayButton.disabled = true;
+            this.gameResultText.textContent = resultText;
+        }
+        const startNewGameBtn = document.createElement('button');
+        startNewGameBtn.textContent = 'Start New Game';
+        if(this.selectionsSection !== null) {
+            this.selectionsSection.append(startNewGameBtn);
+            startNewGameBtn.addEventListener('click', () => {
+                this.resetBoard(startNewGameBtn);
+                this.startNewGame();
+            });
+        }
     }
 }
 
