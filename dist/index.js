8e1104858a502efe17a6e97ecb859b6ed064366b
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-12-31 18:03:48 -0800

    Added reset logic

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 class Player {
     constructor() {
         this.hand = [];
+        this.cardElements = [];
         this.total = 0;
     }
 }
@@ -9,6 +10,7 @@ class Dealer {
     constructor() {
         this.hand = [];
         this.total = 0;
+        this.cardElements = [];
     }
 }
 class Card {
@@ -27,6 +29,7 @@ class Deck {
         return new Card(suit, rank);
     }
     generateDeck() {
+        this.cards = [];
         for (const suit of this.suits) {
             for (const rank of this.ranks) {
                 this.cards.push(this.createCard(suit, rank));
@@ -34,7 +37,7 @@ class Deck {
         }
     }
     getCardIndex() {
-        const randomNumber = Math.floor(Math.random() * this.cards.length) + 1;
+        const randomNumber = Math.floor(Math.random() * this.cards.length);
         return randomNumber;
     }
 }
@@ -47,8 +50,11 @@ class Game {
         this.playerSection = document.querySelector('#player-section');
         this.dealerScoreText = document.querySelector('#dealer-score');
         this.dealerSection = document.querySelector('#dealer-section');
+        this.selectionsSection = document.querySelector('#selections-section');
         this.hitButton = document.querySelector('#hit-button');
         this.stayButton = document.querySelector('#stay-button');
+        this.gameResultText = document.querySelector('#game-result-text');
+        this.dealerFaceDownCard = document.querySelector('#dealer-face-down-card');
         if (this.hitButton !== null) {
             this.hitButton.addEventListener('click', () => {
                 this.drawPlayerCard();
@@ -56,24 +62,26 @@ class Game {
         }
         if (this.stayButton !== null) {
             this.stayButton.addEventListener('click', () => {
-                console.log('clicked stay');
+                this.initiateDealerTurn();
             });
         }
     }
     startNewGame() {
-        var _a;
+        if (this.dealerFaceDownCard !== null) {
+            this.dealerFaceDownCard.style.display = 'block';
+        }
+        this.player.total = 0;
+        this.dealer.total = 0;
+        this.player.hand = [];
+        this.dealer.hand = [];
         this.deck.generateDeck();
         this.drawPlayerCard();
         this.drawPlayerCard();
-        const dealerFaceDownCard = document.createElement('div');
-        dealerFaceDownCard.classList.add('card-container');
-        if (this.dealerSection !== null) {
-            (_a = this.dealerSection) === null || _a === void 0 ? void 0 : _a.append(dealerFaceDownCard);
-        }
         this.drawDealerCard();
     }
     getRankValue(rank, currentTurn) {
         const numericValueRank = typeof rank === 'number' ? rank : 0;
+        console.log(rank, numericValueRank);
         switch (rank) {
             case 'A':
                 (currentTurn.total + 11) > 21 ? currentTurn.total += 1 : currentTurn.total += 11;
@@ -95,6 +103,33 @@ class Game {
                 this.dealerScoreText.textContent = this.dealer.total.toString();
             }
         }
+        this.checkTotals(currentTurn);
+    }
+    initiateDealerTurn() {
+        if (this.dealerFaceDownCard !== null) {
+            this.dealerFaceDownCard.style.display = 'none';
+        }
+        if (this.stayButton !== null && this.hitButton !== null) {
+            this.hitButton.disabled = true;
+            this.stayButton.disabled = true;
+        }
+        if (this.dealer.total < 17) {
+            this.drawDealerCard();
+        }
+    }
+    resetBoard(startNewGameBtn) {
+        this.player.cardElements.forEach((card) => {
+            card.remove();
+        });
+        this.dealer.cardElements.forEach((card) => {
+            card.remove();
+        });
+        startNewGameBtn.remove();
+        if (this.hitButton && this.stayButton && this.gameResultText) {
+            this.hitButton.disabled = false;
+            this.stayButton.disabled = false;
+            this.gameResultText.textContent = '';
+        }
     }
     drawPlayerCard() {
         const index = this.deck.getCardIndex();
@@ -102,6 +137,7 @@ class Game {
         this.player.hand.push(newCard);
         this.getRankValue(newCard.rank, this.player);
         const cardContainer = document.createElement('div');
+        this.player.cardElements.push(cardContainer);
         if (this.playerSection !== null) {
             this.playerSection.append(cardContainer);
             cardContainer.classList.add('card-container');
@@ -122,6 +158,7 @@ class Game {
         this.dealer.hand.push(newCard);
         this.getRankValue(newCard.rank, this.dealer);
         const cardContainer = document.createElement('div');
+        this.dealer.cardElements.push(cardContainer);
         if (this.dealerSection !== null) {
             this.dealerSection.append(cardContainer);
             cardContainer.classList.add('card-container');
@@ -135,6 +172,34 @@ class Game {
             cardContainer.append(cardRank);
         }
         this.deck.cards.splice(index, 1);
+    }
+    checkTotals(currentTurn) {
+        if (currentTurn === this.player) {
+            if (this.player.total > 21) {
+                this.endGame('You lose, better luck next time!');
+            }
+        }
+        if (currentTurn === this.dealer) {
+            if (this.dealer.total > 21) {
+                this.endGame('You win, well done!');
+            }
+        }
+    }
+    endGame(resultText) {
+        if (this.hitButton !== null && this.stayButton !== null && this.gameResultText !== null) {
+            this.hitButton.disabled = true;
+            this.stayButton.disabled = true;
+            this.gameResultText.textContent = resultText;
+        }
+        const startNewGameBtn = document.createElement('button');
+        startNewGameBtn.textContent = 'Start New Game';
+        if (this.selectionsSection !== null) {
+            this.selectionsSection.append(startNewGameBtn);
+            startNewGameBtn.addEventListener('click', () => {
+                this.resetBoard(startNewGameBtn);
+                this.startNewGame();
+            });
+        }
     }
 }
 const game = new Game();
