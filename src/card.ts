b9d4160980566f858c7be47b0611587c6657bb7a
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-01-28 15:47:55 -0800

    Cleaned up formatting

--- src/card.ts
+++ src/card.ts
@@ -1,7 +1,7 @@
 class Card {
     suit: string;
-    rank: (string|number);
-    constructor(suit: string, rank: (string|number)) {
+    rank: string | number;
+    constructor(suit: string, rank: string | number) {
         this.suit = suit;
         this.rank = rank;
     }
